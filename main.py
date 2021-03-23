a4ca841f837a8b13e16072e59132d5c7d2ca7302
Oliver0804 <icetzsr@gmail.com>
2021-03-24 00:29:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import mariadb
 import sys
-
+import time
 conn= mariadb.connect(user="airbox",password="password",host="127.0.0.1",port=3306,database="db_airbox")
 cur=conn.cursor()
 cur.execute("select version()")
 data =cur.fetchone()
 print (data)
 
-
-cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES ('2021-03-31 00:00:00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')")
+localtime = time.localtime()
+result = time.strftime("%Y-%m-%d %I:%M:%S", localtime)
+print(result)
+cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES (str(result), '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')")
 conn.autocommit=True
