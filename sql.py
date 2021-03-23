f4da62c04b95eb883a4d1b158f600312cc2308b4
Oliver0804 <icetzsr@gmail.com>
2021-03-24 00:19:10 +0800

    Update sql.py
    add time

--- sql.py
+++ sql.py
@@ -1,6 +1,7 @@
 # Module Imports
 import mariadb
 import sys
+import time
 
 # Connect to MariaDB Platform
 try:
@@ -20,7 +21,10 @@ cur = conn.cursor()
 
 #insert information
 try: 
-    cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES ('?', '?', '?', '?', '?', '?','?','?','?','?','?','?','?','?','?');",("2021-03-22 00:12:34",20,30,40,50,60,70,80,90,100,110,111,112,113,114))
+    localtime = time.localtime()
+    result = time.strftime("%Y-%m-%d %I:%M:%S", localtime)
+    print(result)
+    cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES ('?', '?', '?', '?', '?', '?','?','?','?','?','?','?','?','?','?');",(result,20,30,40,50,60,70,80,90,100,110,111,112,113,114))
 except mariadb.Error as e: 
     print(f"Error: {e}") 
 conn.commit() 
