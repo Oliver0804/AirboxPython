e9810303b0ea18fdf3ac9d11b63c3f05416438f1
Oliver0804 <icetzsr@gmail.com>
2021-03-23 23:18:42 +0800

    Update main.py
    change autocommit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ cur.execute("select version()")
 data =cur.fetchone()
 print (data)
 
-conn.autocommit=True
-cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES ('2021-03-31 00:00:00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')")
 
+cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES ('2021-03-31 00:00:00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')")
+conn.autocommit=True
