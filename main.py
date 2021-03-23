e95b5ade05e641ed6ddb8a608a4df05bd9a23e02
oliver0804 <icetzsr@gmail.com>
2021-03-24 00:35:05 +0800

    可以工作

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ print (data)
 localtime = time.localtime()
 result = time.strftime("%Y-%m-%d %I:%M:%S", localtime)
 print(result)
-cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES (str(result), '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')")
+cur.execute("INSERT INTO `Data` (`time`, `pm10`, `formaldehyde`, `vocs`, `co2`, `temp`, `humidity`, `battery`, `runtime`, `air3um`, `air5um`, `air10um`, `air25um`, `air50um`, `air100um`) VALUES ('2020-04-12 12:34:45', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')")
 conn.autocommit=True
